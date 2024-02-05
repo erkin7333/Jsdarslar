161a0017cc373a76df85a4fe6bb82e8b9c71df86
erkin7333 <erkinnajimov90@gmail.com>
2024-02-05 12:46:49 +0500

    If else

--- js/if_operator_uchun.js
+++ js/if_operator_uchun.js
@@ -1,1 +1,9 @@
-alert(1)
+let age = prompt("Yoshingiz nechida?")
+
+if (age < 18){
+    alert("Hali voxlik")
+} else if (age > 40){
+    alert("Uje siz rahmatlisiz")
+} else {
+    alert("Xush kelibsiz")
+}

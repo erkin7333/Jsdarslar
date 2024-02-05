9eb9ce65c38bba2c5dde591dfae20395944fa860
erkin7333 <erkinnajimov90@gmail.com>
2024-02-05 13:56:21 +0500

    Switch case

--- js/if_operator_uchun.js
+++ js/if_operator_uchun.js
@@ -1,9 +1,26 @@
-let age = prompt("Yoshingiz nechida?")
+// let age = prompt("Yoshingiz nechida?")
+//
+// if (age < 18){
+//     alert("Hali voxlik")
+// } else if (age > 40){
+//     alert("Uje siz rahmatlisiz")
+// } else {
+//     alert("Xush kelibsiz")
+// }
 
-if (age < 18){
-    alert("Hali voxlik")
-} else if (age > 40){
-    alert("Uje siz rahmatlisiz")
-} else {
-    alert("Xush kelibsiz")
+
+let age = prompt("Yoshigizni kiriting!")
+switch (age){
+    case "18":
+        alert("Sizni yoshingiz 18da")
+        break
+    case "50":
+        alert("Sizni yoshingiz 50da")
+        break
+    case "25":
+        alert("Sizni yoshingiz 25da")
+        break
+    default:
+        alert('Sizni yoshiz nomalum')
+        break
 }

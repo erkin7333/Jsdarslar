0cf2ddb438556ff0335f5a1e35e80f6acfb828cd
erkin7333 <erkinnajimov90@gmail.com>
2024-02-05 12:27:13 +0500

    Kichkina praktika qilindi uchala usulda ham

--- js/script1.js
+++ js/script1.js
@@ -14,3 +14,51 @@
 
 // Javascriptda for bilan ishlashni uchinchi usuli
 
+// let i = 0
+// while (i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+// Forda kichik praktika
+// Birinchi usulda praktik
+// let answers = []
+// let questions = [
+//     "Salom isming nima?",
+//     "Telefon raqamingiz nechi?",
+//     "Yoshingiz nechida?"
+// ]
+//
+// for (i = 0; i < questions.length; i++) {
+//     answers[i] = prompt(questions[i])
+// }
+// console.log(answers)
+
+// Ikkinchi usulda praktika
+
+// let answers = []
+// let questions = [
+//     "Salom ismingizni kiriting?",
+//     "Telefon raqam kiriting",
+//     "Yoshingizni kiriting"
+// ]
+// let i = 0
+// do {
+//     console.log(answers[i] = prompt(questions[i]))
+//     i++
+// }while (i < questions.length)
+// console.log(answers)
+
+// Uchinchi usul yordamida praktika qilish
+// let answers = []
+// let questions = [
+//     "Kompuyter nomini kiriting?",
+//     "Dasturlash tilini kiriting",
+//     "Soat necha"
+// ]
+// let i = 0
+// while (i < questions.length) {
+//     console.log(answers[i] = prompt(questions[i]))
+//     i++
+// }
+// console.log(answers)

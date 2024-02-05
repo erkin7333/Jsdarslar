21c278f7d7a5ec4172b0531767037429f7690373
erkin7333 <erkinnajimov90@gmail.com>
2024-02-05 11:21:41 +0500

    Js dars-1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('Erkin')
+
+let a = 10
+let b = 10
+console.log(a == b)
+
+
+console.log('10' === 10)
+
+console.log(-4/0)
+
+console.log('4' + 4)
+
+let answers = confirm("Sizi yoshiz 20mi")
+console.log(answers)

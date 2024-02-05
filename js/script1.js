91c764e64875a20b14022e39954f5f3c061aa316
erkin7333 <erkinnajimov90@gmail.com>
2024-02-05 11:37:27 +0500

    For bilan ishlash

---
+++ js/script1.js
@@ -0,0 +1,16 @@
+
+// Javascriptda for bilan ishlashni birinchi usuli
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// Javascriptda for bilan ishlashni ikkinchi usuli
+
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++
+// }while (i <= 10)
+
+// Javascriptda for bilan ishlashni uchinchi usuli
+

64d91508f228979005cb81d435ba00d8499440bb
erkin7333 <erkinnajimov90@gmail.com>
2024-02-06 17:41:13 +0500

    apply va call uchun tushuncha

--- dars2/js/script.js
+++ dars2/js/script.js
@@ -1,1 +1,41 @@
-alert(2)
+// function sam(){
+//     console.log(this)
+// }
+//
+// // Object ichidagi context this nimaga teng desa?
+// // Object ichidagi this objectni o'ziga teng
+//
+// const persone = {
+//     name: "Erkin",
+//     age: 25,
+//     job: "Angular Js DEVELOPER",
+//     callContext: sam,
+//     callAnotherContext: sam.bind(this),
+//     callInfoJob: function (number) {
+//         console.group(`${this.name} info: `)
+//         console.log(`Name is ${this.name}`)
+//         console.log(`Age is ${this.age}`)
+//         console.log(`Job is ${this.job}`)
+//         console.log(`Contact is ${number}`)
+//         console.groupEnd()
+//     }
+// }
+//
+// const frontendDev = {
+//     name: 'Maruf',
+//     age: 22,
+//     job: "Back-end developer"
+// }
+
+// persone.callInfoJob.bind(frontendDev)();
+
+// call va apply
+
+// const fullInfo = persone.callInfoJob.bind(frontendDev);
+// fullInfo('+998974347333')
+
+// apply bilan ishlashda doim parametrga to'rburchak qovs bilan beriladi
+// persone.callInfoJob.apply(frontendDev, ['+9989712345676'])
+
+// call
+// persone.callInfoJob.call(frontendDev, '+9987945352165')
